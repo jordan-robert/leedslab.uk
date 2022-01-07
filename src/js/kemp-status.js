adf0175a1b85441fa1e95eeb2e5d1f6d0f52f7d1
Jrob <66264477+jordan-robert@users.noreply.github.com>
2022-01-07 23:57:59 +0000

    Update kemp-status.js

--- src/js/kemp-status.js
+++ src/js/kemp-status.js
@@ -3,8 +3,9 @@ function imageFound() {
 }
 
 function imageNotFound() {
-     document.getElementById("kemp-fail-status").innerHTML = "KEMP Offline!"; 
-	 alert("The main load balancer (KEMP) is not reachable from your PC. Because of this, both servers will be offline. This is normally a WAN or server issue and should be sorted soon, sorry!") 
+     document.getElementById("kemp-fail-status").innerHTML = "KEMP Offline! (All VMs will be down, as they connect to this!)"; 
+     document.getElementById("kemp-fail-banner").innerHTML = "Kemp Server Offline > Click for details";
+     location.href = "#popup1";
 }
 
 ServerTest("https://kemp.leedslab.uk/lm_auth_proxy?LMimage=con-test.png");
