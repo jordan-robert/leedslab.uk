ae3c8de0c5595b751ae8b3c06962da3b728f8606
Jrob <66264477+jordan-robert@users.noreply.github.com>
2022-01-15 00:34:29 +0000

    Add files via upload

---
+++ src/js/azure-eve-status.js
@@ -0,0 +1,32 @@
+function ServerTest(url) {
+    var tester=new Image();
+    tester.onload=imageFound;
+    tester.onerror=imageNotFound;
+    tester.src=url;
+}
+
+var today = new Date();
+var time = today.getHours()
+
+if (time < 7) {
+  Shutdownstate = "Morning, you're too early, Azure EVE will be online at 07:00AM!";
+} else if (time > 19) {
+  Shutdownstate = "Azure EVE has shutdown for the night and will be back at 07:00AM, have a good evening!";
+} else {
+  function imageFound() {
+    document.getElementById("azure-status").innerHTML = "Azure EVE Online";
+  }
+function imageNotFound() {
+     document.getElementById("azure-fail-status").innerHTML = "Azure EVE Offline"; 
+     location.href = "#popup1";
+}
+
+var apollo = "https://azure-eve1.leedslab.uk/themes/adminLTE/connectivity/aeve/";
+var ram = Math.floor(Math.random()* 2000);
+var svg = ".svg";
+var url = apollo + ram + svg;
+
+ServerTest(url);
+  
+}
+document.getElementById("azure-shutdown").innerHTML = Shutdownstate;
