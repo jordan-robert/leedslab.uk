97cd6b79b8b9c1a5b2ddb43e544902069251b3a4
Jrob <66264477+jordan-robert@users.noreply.github.com>
2022-04-19 15:44:05 +0100

    Update zeus-status.js

--- src/js/zeus-status.js
+++ src/js/zeus-status.js
@@ -1,12 +1,3 @@
-var timeleft = 13;
-var downloadTimer = setInterval(function(){
-  if(timeleft <= 0){
-    clearInterval(downloadTimer);
-  }
-  document.getElementById("progressBar").value = 10 - timeleft;
-  timeleft -= 1;
-}, 1000);
-
 document.getElementById("zeus-fail-status").innerHTML = "Please Wait..."; 
 var zeus = "https://zcon.leedslab.uk/themes/adminLTE/connectivity/";
 var ram = Math.floor(Math.random()* 2000);
