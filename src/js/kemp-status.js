86810cad76ee0c16457f53f8eabd6e4e42c0e4bc
Jrob <66264477+jordan-robert@users.noreply.github.com>
2022-04-19 15:45:12 +0100

    Update kemp-status.js

--- src/js/kemp-status.js
+++ src/js/kemp-status.js
@@ -1,10 +1,20 @@
+var timeleft = 13;
+var downloadTimer = setInterval(function(){
+  if(timeleft <= 0){
+    clearInterval(downloadTimer);
+  }
+  document.getElementById("progressBar").value = 10 - timeleft;
+  timeleft -= 1;
+}, 1000);
 document.getElementById("kemp-fail-status").innerHTML = "Testing Connectivity to Lab Servers...."; 
 function imageFound() {
+    document.getElementById("progressBar").style.display='none';
     document.getElementById("kemp-status").innerHTML = "Kemp LB Online"; 
 }
 
 function imageNotFound() {
-     document.getElementById("kemp-fail-status").innerHTML = "Core LoadBalancer Offline! (All VMs will be down, as they connect to this!)"; 
+     document.getElementById("progressBar").style.display='none';
+     document.getElementById("kemp-fail-status").innerHTML = "Core LoadBalancer Offline! (All VMs will be down, as they connect to this!)";
      document.getElementById("kemp-fail-banner").innerHTML = "Kemp Server Offline > Click for details";
      location.href = "#popup1";
 }
