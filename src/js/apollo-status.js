9bb03a60d2a6d28fc19ac3ccae4a00db9abca36e
Jrob <66264477+jordan-robert@users.noreply.github.com>
2022-04-19 12:20:02 +0100

    Update apollo-status.js

--- src/js/apollo-status.js
+++ src/js/apollo-status.js
@@ -1,3 +1,4 @@
+document.getElementById("apollo-fail-status").innerHTML = "Please Wait..."; 
 function imageFound() {
     document.getElementById("apollo-status").innerHTML = "Apollo Online";
     document.getElementById("apollo-status-banner").innerHTML = "Apollo Online"; 
