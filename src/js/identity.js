0859f6dce7ba5dfde40f707f308a0325034e8f7b
Jrob <66264477+jordan-robert@users.noreply.github.com>
2022-07-23 19:25:33 +0100

    Update identity.js

--- src/js/identity.js
+++ src/js/identity.js
@@ -1,4 +1,4 @@
-const hour = new Date().getHours(); 
+const hour = new Date().getHours();
 $.get('https://start.leedslab.uk/cdn-cgi/access/get-identity?format=json', function(data) {
   console.log(data);
 name = (data.name)
