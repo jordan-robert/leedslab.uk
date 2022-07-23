f67f631d5970349e0fc38481a7eaeb0d1ff0596e
Jrob <66264477+jordan-robert@users.noreply.github.com>
2022-07-24 00:05:48 +0100

    Update identity.js

--- src/js/identity.js
+++ src/js/identity.js
@@ -22,11 +22,11 @@ document.getElementById("ip-print").innerHTML = ip;
 if (nameif === true) {
   let text = (data.name);
   const myArray = text.split(",");
-  document.getElementById("name1").innerHTML = myArray[2];
+  document.getElementById("print-name").innerHTML = myArray[2];
 } else {
   let text = (data.name);
   const myArray = text.split(" ");
-  document.getElementById("name1").innerHTML = myArray[0];
+  document.getElementById("print-name").innerHTML = myArray[0];
 }
 
   
