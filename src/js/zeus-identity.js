fb6478f5bc01b536b4d072e28d7d5616ecab65d2
Jrob <66264477+jordan-robert@users.noreply.github.com>
2022-07-24 13:29:30 +0100

    Update zeus-identity.js

--- src/js/zeus-identity.js
+++ src/js/zeus-identity.js
@@ -12,7 +12,6 @@ $.get('https://zta-zeus.leedslab.uk/cdn-cgi/access/get-identity?format=json', fu
   console.log(data);
 name = (data.name)
 email = (data.email)
-ip = (data.ip)
 name.toString
 nameif = name.includes(",");
 document.getElementById("name-print").innerHTML = name;
@@ -22,13 +21,12 @@ document.getElementById("ip-print").innerHTML = ip;
 if (nameif === true) {
   let text = (data.name);
   const myArray = text.split(",");
-  document.getElementById("print-name").innerHTML = myArray[2];
+  document.getElementById("print-first-name").innerHTML = myArray[2];
 } else {
   let text = (data.name);
   const myArray = text.split(" ");
-  document.getElementById("print-name").innerHTML = myArray[0];
+  document.getElementById("print-first-name").innerHTML = myArray[0];
 }
 
   
-        $("#gfg").html(data.name);
 });
